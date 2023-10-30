2ad1908a81753eecba45fc3916a070d45c6c9023
juliaDzi <juliaDzi@users.noreply.github.com>
2023-10-30 20:06:42 +0000

    Initial commit with Empty Actions

---
+++ app/src/main/java/com/griffith/mybuddy/Profile.kt
@@ -0,0 +1,13 @@
+package com.griffith.mybuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class Profile : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
